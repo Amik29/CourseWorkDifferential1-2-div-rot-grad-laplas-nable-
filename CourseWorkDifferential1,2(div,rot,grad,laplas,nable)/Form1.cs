e6baa71b065a620c46d2e33f82690b6a7ce15f4c
amik29 <amik29@DESKTOP-63OJVQ8>
2022-09-29 22:57:14 +0500

    Добавьте файлы проекта.

---
+++ CourseWorkDifferential1,2(div,rot,grad,laplas,nable)/Form1.cs
@@ -0,0 +1,10 @@
+namespace CourseWorkDifferential1_2_div_rot_grad_laplas_nable_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
